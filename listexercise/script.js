6d05b9f6e772502e313805a3cc222ced7d9fd161
jeremyrayjewell <jeremyrayjewell@gmail.com>
2024-03-23 01:03:11 -0300

    first commit

--- listexercise/script.js
+++
@@ -1,9 +0,0 @@
-var button = document.getElementById("enter");
-var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
-
-button.addEventListener("click", function(){
-    var li = document.createElement('li');
-    li.appendChild(document.createTextNode('testing'));
-    ul.appendChild(li)
-})
